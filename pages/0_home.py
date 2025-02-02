0717d0c8838cf767c03d53ef98c9e37d7a37e8fb
Raknaros <giumarchand@gmail.com>
2025-02-02 16:32:32 -0500

    First Commit

--- pages/0_home.py
+++ pages/0_home.py
@@ -15,29 +15,33 @@ if st.session_state.get("authentication_status"):
     st.title('Bienvenido')
     col1, col2 = st.columns([1, 6])
     #st.session_state['authenticator'].logout(location='sidebar', button_name='Cerrar Sesion')
+
+    def info_resumen(alias=None):
+        if alias == None:
+            pass
+        else:
+            entidad = st.session_state.entidades.loc[st.session_state.entidades['alias'] == alias]
+            st.write("**RUC:** " + str(entidad['ruc'].values[0]))
+            st.write("**Usuario SOL:** " + str(entidad['usuario_sol'].values[0]))
+            st.write("**Clave SOL:** " + str(entidad['clave_sol'].values[0]))
+
+
+    with col1.container(height=800, border=False):
+        search_query = st.text_input("Buscar entidad")
+        entidades_filtradas = st.session_state.entidades.loc[
+            st.session_state.entidades['alias'].str.contains(search_query, case=False) & (
+                        st.session_state.entidades['activo'] == True)]
+        for entidad in entidades_filtradas['alias'].tolist():
+            if st.button(entidad, use_container_width=True, type="primary", key=entidad):
+                st.session_state.entidad_seleccionada = entidad
+
     with col2:
         tab1, tab2, tab3, tab4, tab5 = st.tabs(["Resumen", "Ventas", "Compras", "Declaraciones", "Pagos"])
         with tab1:
             icol1, icol2, icol3, icol4, icol5, icol6, icol7, icol8 = st.columns(8)
-
-
-            def info_resumen(alias=None):
-                if alias == None:
-                    pass
-                else:
-                    entidad = st.session_state.entidades.loc[st.session_state.entidades['alias'] == alias]
-                    icol1.write("**" + str(entidad['ruc'].values.flatten()) + "**")
-                    icol1.write("**" + str(entidad['usuario_sol'].values) + "**")
-
-    with col1.container(height=800, border=False):
-        st.header("ENTIDADES")
-        for entidad in st.session_state.entidades.loc[st.session_state.entidades['activo'] == True]['alias'].tolist():
-            if not st.session_state.entidades.loc[st.session_state.entidades['alias'] == entidad, 'activo'].iloc[0]:
-                st.button(entidad, use_container_width=True)
-            else:
-                st.button(entidad, use_container_width=True, type="primary", on_click=info_resumen(entidad))
-
-        #st.selectbox("Elija una entidad", )
+            with icol1:
+                if 'entidad_seleccionada' in st.session_state:
+                    info_resumen(st.session_state.entidad_seleccionada)
 
 
 
